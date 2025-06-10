715ea5d41a9ef31166e9aa4ea2cdc7427006f180
이규민 <kyumin991105@duksung.ac.kr>
2025-06-10 21:57:55 +0900

    ✨ [FEAT] 인증번호 로직 구현 완료, 예외처리 구현 완료

--- src/main/java/com/ceos/beatbuddy/domain/member/dto/BusinessMemberResponseDTO.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/dto/BusinessMemberResponseDTO.java
@@ -2,11 +2,12 @@ package com.ceos.beatbuddy.domain.member.dto;
 
 import com.ceos.beatbuddy.domain.member.entity.Member;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BusinessMemberResponseDTO {
     private String realName;
