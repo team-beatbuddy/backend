95f52583b9ed6b624cc610bb6687dad02bdb7211
이규민 <kyumin991105@duksung.ac.kr>
2025-06-16 23:10:35 +0900

    ✨ [FEAT] 행사 정보에 맞게 정보 수집 되로록 구현

---
+++ src/main/java/com/ceos/beatbuddy/domain/event/dto/EventAttendanceRequestDTO.java
@@ -0,0 +1,49 @@
+package com.ceos.beatbuddy.domain.event.dto;
+
+import com.ceos.beatbuddy.domain.event.entity.Event;
+import com.ceos.beatbuddy.domain.event.entity.EventAttendance;
+import com.ceos.beatbuddy.domain.event.entity.EventAttendanceId;
+import com.ceos.beatbuddy.domain.member.constant.Gender;
+import com.ceos.beatbuddy.domain.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventAttendanceRequestDTO {
+
+    @Schema(description = "이벤트 아이디")
+    @NotNull(message = "이벤트 아이디는 필수입니다.")
+    private Long eventId;
+    private String name;
+    private String gender;
+    private String phoneNumber;
+    private Integer totalNumber;
+    private boolean isPaid;
+    private String snsType;
+    private String snsId;
+
+    public static EventAttendance toEntity(EventAttendanceRequestDTO dto, Member member, Event event) {
+        EventAttendance.EventAttendanceBuilder builder = EventAttendance.builder()
+                .id(new EventAttendanceId(dto.getEventId(), member.getId()))
+                .event(event)
+                .member(member);
+
+        if (dto.getName() != null) builder.name(dto.getName());
+        if (dto.getGender() != null) builder.gender(Gender.valueOf(dto.getGender()));
+        if (dto.getPhoneNumber() != null) builder.phoneNumber(dto.getPhoneNumber());
+        if (dto.getTotalNumber() != null) builder.totalMember(dto.getTotalNumber());
+        if (dto.getSnsType() != null) builder.snsType(dto.getSnsType());
+        if (dto.getSnsId() != null) builder.snsId(dto.getSnsId());
+        builder.hasPaid(dto.isPaid());
+
+        return builder.build();
+    }
+
+}
---
+++ src/main/java/com/ceos/beatbuddy/domain/event/exception/EventErrorCode.java
@@ -0,0 +1,35 @@
+package com.ceos.beatbuddy.domain.event.exception;
+
+import com.ceos.beatbuddy.domain.member.exception.MemberErrorCode;
+import com.ceos.beatbuddy.global.ApiCode;
+import org.springframework.http.HttpStatus;
+
+public enum EventErrorCode implements ApiCode {
+    NOT_FOUND_EVENT(HttpStatus.NOT_FOUND, "존재하지 않는 이벤트입니다."),
+    ALREADY_ATTENDANCE_EVENT(HttpStatus.CONFLICT, "이미 참여 신청한 이벤트입니다."),
+    MISSING_NAME(HttpStatus.BAD_REQUEST, "이름 입력은 필수입니다."),
+    MISSING_GENDER(HttpStatus.BAD_REQUEST, "성별 입력은 필수입니다."),
+    MISSING_PHONE(HttpStatus.BAD_REQUEST, "핸드폰 번호 입력은 필수입니다."),
+    MISSING_TOTAL_COUNT(HttpStatus.BAD_REQUEST, "동행인원 입력은 필수입니다."),
+    MISSING_PAYMENT(HttpStatus.BAD_REQUEST, "지불 완료 입력은 필수입니다."),
+    MISSING_SNS_ID_OR_TYPE(HttpStatus.BAD_REQUEST, "SNS ID 또는 TYPE 입력은 필수입니다."),
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+    EventErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return this.httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
