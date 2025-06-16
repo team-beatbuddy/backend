52ee026ea7af73fb4a95d4e1ea1a03b59e2d2ece
이규민 <kyumin991105@duksung.ac.kr>
2025-06-16 18:57:51 +0900

    🐛 [FIX] 좋아요만 감소가 있어서 음수 안 되도록 처리

--- src/main/java/com/ceos/beatbuddy/domain/magazine/application/MagazineService.java
+++ src/main/java/com/ceos/beatbuddy/domain/magazine/application/MagazineService.java
@@ -182,7 +182,6 @@ public class MagazineService {
         );
 
         magazineScrapRepository.delete(magazineScrap);
-        magazine.decreaseLike();
 
         return MagazineDetailDTO.toDTO(magazine);
 
