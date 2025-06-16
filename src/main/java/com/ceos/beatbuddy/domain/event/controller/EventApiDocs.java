00b7bd79c9c34e0acf567db21e53e38c2b35c1c4
이규민 <kyumin991105@duksung.ac.kr>
2025-06-17 02:45:29 +0900

    ✨ [FEAT] 참석 명단 반환 구현

--- src/main/java/com/ceos/beatbuddy/domain/event/controller/EventApiDocs.java
+++ src/main/java/com/ceos/beatbuddy/domain/event/controller/EventApiDocs.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface EventApiDocs {
     @Operation(summary = "이벤트 작성 기능\n",
             description = "admin과 business 멤버에 한해서만 이벤트를 작성할 수 있도록 해두었습니다. (추후 변경 가능), 데이터 전달은 multipart/form-data이며 'eventCreateRequestDTO'는 JSON 문자열 형태로 전송해야 합니다.")
@@ -301,4 +303,81 @@ public interface EventApiDocs {
     ResponseEntity<ResponseDTO<EventListResponseDTO>> getEventUpcomingPopular (@PathVariable String sort,
                                                                                 @RequestParam(defaultValue = "1") Integer page,
                                                                                 @RequestParam(defaultValue = "10") Integer size);
+
+    @Operation(summary = "곧 진행될 이벤트",
+            description = "오늘부터 앞으로의 이벤트를 보여줍니다. sort 에는 popular / latest / region을 넣을 수 있으나 현재는 region은 구현되어있지 않습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "곧 다가올 이벤트", content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(name = "곧 다가올 이벤트 조회 성공", value = """
+                    {
+                      "status": 200,
+                      "code": "SUCCESS_GET_UPCOMING_EVENT",
+                      "message": "이벤트가 성공적으로 조회되었습니다.",
+                      "data": {
+                        "sort": "popular",
+                        "page": 1,
+                        "size": 10,
+                        "totalSize": 2,
+                        "eventResponseDTOS": [
+                          {
+                            "eventId": 1,
+                            "title": "이벤트 시작",
+                            "content": "이게 바로 이트",
+                            "image": "https://beatbuddy.s3.ap-northeast-2.amazonaws.com/ddded007-dGroup%201000003259.png",
+                            "likes": 5,
+                            "views": 0,
+                            "scraps": 0,
+                            "startDate": "2025-06-17",
+                            "endDate": "2025-06-17",
+                            "dday": "D-0",
+                            "location": "경기도 파주"
+                          },
+                          {
+                            "eventId": 2,
+                            "title": "string",
+                            "content": "string",
+                            "image": "https://beatbuddy.s3.ap-northeast-2.amazonaws.com/ae7cd814-fGroup%201000003259.png",
+                            "likes": 1,
+                            "views": 0,
+                            "scraps": 0,
+                            "startDate": "2025-06-18",
+                            "endDate": "2025-06-23",
+                            "dday": "D-1",
+                            "location": "서울시"
+                            
+                          }
+                        ]
+                      }
+                    }
+             """)
+            )),
+            @ApiResponse(responseCode = "404", description = "이벤트 또는 유저 정보 없음", content = @Content(
+                    mediaType = "application/json",
+                    examples = {
+                            @ExampleObject(name = "유저 없음", value = """
+                        {
+                          "status": 404,
+                          "error": "NOT_FOUND",
+                          "code": "MEMBER_NOT_EXIST",
+                          "message": "요청한 유저가 존재하지 않습니다."
+                        }
+                        """)
+                    }
+            )),
+            @ApiResponse(responseCode = "403", description = "이벤트를 작성한 유저가 아님", content = @Content(
+                    mediaType = "application/json",
+                    examples = {
+                            @ExampleObject(name = "유저 권한 없음", value = """
+                        {
+                          "status": 403,
+                          "error": "FORBIDDEN",
+                          "code": "FORBIDDEN_EVENT_ACCESS",
+                          "message": "해당 이벤트에 대한 접근 권한이 없습니다."
+                        }
+                        """)
+                    }
+            ))
+    })
+    ResponseEntity<ResponseDTO<EventAttendanceExportListDTO>> getEventAttendanceList(@PathVariable Long eventId);
 }
