351687577bd487b1950057ebfd39ac80886dcb74
이규민 <kyumin991105@duksung.ac.kr>
2025-06-16 16:42:33 +0900

    ✨ [FEAT] 좋아요를 이미 눌렀을 대 예외처리 진행

--- src/main/java/com/ceos/beatbuddy/domain/magazine/exception/MagazineErrorCode.java
+++ src/main/java/com/ceos/beatbuddy/domain/magazine/exception/MagazineErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum MagazineErrorCode implements ApiCode {
     CANNOT_ADD_MAGAZINE_UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "글을 작성할 수 없는 유저입니다."),
     MAGAZINE_NOT_EXIST(HttpStatus.UNAUTHORIZED, "해당 매거진을 찾을 수 없습니다."),
-
+    ALREADY_LIKE_MAGAZINE(HttpStatus.CONFLICT, "이미 좋아요를 누른 매거진입니다."),
     ;
 //    POST_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다."),
 //    MEMBER_NOT_MATCH(HttpStatus.BAD_REQUEST, "게시물의 생성자가 아닙니다."),
