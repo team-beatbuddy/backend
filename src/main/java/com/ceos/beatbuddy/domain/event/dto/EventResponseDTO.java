1deb0372fde6e36013448f3c26496dd6731d6d3e
이규민 <kyumin991105@duksung.ac.kr>
2025-06-16 22:15:47 +0900

    ✨ [FEAT] 이벤트 참석 정보 수집 여부 필드 추가

--- src/main/java/com/ceos/beatbuddy/domain/event/dto/EventResponseDTO.java
+++ src/main/java/com/ceos/beatbuddy/domain/event/dto/EventResponseDTO.java
@@ -1,6 +1,8 @@
 package com.ceos.beatbuddy.domain.event.dto;
 
 import com.ceos.beatbuddy.domain.event.entity.Event;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,6 +13,14 @@ public class EventResponseDTO {
     private String title;
     private String content;
     private String image;
+    private boolean receiveInfo;
+    private boolean receiveName; // 이름 받을 건지
+    private boolean receiveGender; // 성별 받을 건지
+    private boolean receivePhoneNumber; // 전화번호 받을 건지
+    private boolean receiveTotalCount; // 동행 인원 받을 건지
+    private boolean receiveSNSId; // sns id 받을 건지
+    private boolean receiveMoney; // 예약금 받을 건지
+
 
     public static EventResponseDTO toDTO(Event event) {
         return EventResponseDTO.builder()
@@ -18,6 +28,12 @@ public class EventResponseDTO {
                 .title(event.getTitle())
                 .content(event.getContent())
                 .image(event.getThumbImage())
+                .receiveInfo(event.isReceiveInfo())
+                .receiveName(event.isReceiveName())
+                .receiveGender(event.isReceiveGender())
+                .receivePhoneNumber(event.isReceivePhoneNumber())
+                .receiveMoney(event.isReceiveMoney())
+                .receiveSNSId(event.isReceiveSNSId())
                 .build();
     }
 }
