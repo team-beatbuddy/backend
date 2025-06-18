1eb08537cf9ed0ee5b9459ed3026903fff94e0f0
이규민 <kyumin991105@duksung.ac.kr>
2025-06-18 21:12:19 +0900

    ✨ [FEAT] 나를 팔로우하는 사람 목록 조회 구현 완료

--- src/main/java/com/ceos/beatbuddy/domain/follow/controller/FollowApiDocs.java
+++ src/main/java/com/ceos/beatbuddy/domain/follow/controller/FollowApiDocs.java
@@ -158,7 +158,8 @@ public interface FollowApiDocs {
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ResponseDTO.class),
-                            examples = {@ExampleObject(value = """
+                            examples = {@ExampleObject(name = "내가 팔로우한 사람 조회 성공", value = """
+                                    
                                     {
                                       "status": 200,
                                       "code": "SUCCESS_GET_FOLLOWINGS",
@@ -207,4 +208,61 @@ public interface FollowApiDocs {
             )
     })
     ResponseEntity<ResponseDTO<List<FollowResponseDTO>>> getFollowings();
+
+    @Operation(summary ="나를 팔로우하는 목록 조회 기능\n",
+            description = "나를 팔로우하는 사람들을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "나를 팔로우하는 사람들 조회, 조회했으나 리스트가 비어있는 경우",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDTO.class),
+                            examples = {@ExampleObject(name = "내가 팔로우하는 목록", value = """
+                                    {
+                                      "status": 200,
+                                      "code": "SUCCESS_GET_FOLLOWINGS",
+                                      "message": "내가 팔로우하는 목록을 가져왔습니다.",
+                                      "data": [
+                                        {
+                                          "followerId": 156,
+                                          "followingId": 140
+                                        }
+                                      ]
+                                    }
+                                    """),
+                                    @ExampleObject(
+                                            name = "팔로잉 한 사람이 없는 경우",
+                                            value = """
+                                        {
+                                          "status": 200,
+                                          "code": "SUCCESS_BUT_EMPTY_LIST",
+                                          "message": "성공적으로 조회했으나 리스트가 비었습니다.",
+                                          "data": []
+                                        }
+                    """
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "존재하지 않는 유저인 경우",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples =
+                            @ExampleObject(
+                                    name = "존재하지 않는 유저",
+                                    value = """
+                                                        {
+                                                          "status": 404,
+                                                          "error": "NOT_FOUND",
+                                                          "code": "MEMBER_NOT_EXIST",
+                                                          "message": "요청한 유저가 존재하지 않습니다."
+                                                        }
+                                                    """
+                            )
+
+                    )
+            )
+    })
+    ResponseEntity<ResponseDTO<List<FollowResponseDTO>>> getFollowers();
 }
