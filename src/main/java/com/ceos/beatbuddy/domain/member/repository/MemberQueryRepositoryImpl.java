4266dfab0ef10b6a0ce0f76782484d7c6ca5fffc
이규민 <kyumin991105@duksung.ac.kr>
2025-06-20 13:46:20 +0900

    🐛 [FIX] follow 가 같은 인스턴스로 충돌남 -> 해결

--- src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberQueryRepositoryImpl.java
+++ src/main/java/com/ceos/beatbuddy/domain/member/repository/MemberQueryRepositoryImpl.java
@@ -22,37 +22,38 @@ public class MemberQueryRepositoryImpl implements MemberQueryRepository{
     private final JPAQueryFactory queryFactory;
     @Override
     public MemberProfileSummaryDTO getMemberSummary(Long memberId) {
-        QMember m = member;
-        QPost p = post;
-        QFollow f = follow;
+        QMember qMember = member;
+        QPost qPost = post;
+        QFollow qFollowerFollow = new QFollow("followerFollow");
+        QFollow qFollowingFollow = new QFollow("followingFollow");
 
         Tuple result = queryFactory
                 .select(
-                        m.id,
-                        m.nickname,
-                        m.role,
-                        p.id.countDistinct(),
-                        f.follower.id.countDistinct(),
-                        f.following.id.countDistinct()
+                        qMember.id,
+                        qMember.nickname,
+                        qMember.role,
+                        qPost.id.countDistinct(),
+                        qFollowerFollow.follower.id.countDistinct(),
+                        qFollowingFollow.following.id.countDistinct()
 
                 )
-                .from(m)
-                .leftJoin(p).on(p.member.id.eq(m.id))
-                .leftJoin(f).on(f.follower.id.eq(m.id))
-                .leftJoin(f).on(f.following.id.eq(m.id))
-                .where(m.id.eq(memberId))
-                .groupBy(m.id, m.nickname, m.profileImage, m.role)
+                .from(qMember)
+                .leftJoin(qPost).on(qPost.member.id.eq(qMember.id))
+                .leftJoin(qFollowerFollow).on(qFollowerFollow.follower.id.eq(qMember.id))
+                .leftJoin(qFollowingFollow).on(qFollowingFollow.following.id.eq(qMember.id))
+                .where(qMember.id.eq(memberId))
+                .groupBy(qMember.id, qMember.nickname, qMember.profileImage, qMember.role)
                 .fetchOne();
 
         if (result == null) {
             throw new CustomException(MemberErrorCode.MEMBER_NOT_EXIST);
         } else {
             // null인 경우 0으로 입력되도록 처리
-            int postCount = result.get(p.id.countDistinct()) != null ? result.get(p.id.countDistinct()).intValue() : 0;
-            int followerCount = result.get(f.id.countDistinct()) != null ? result.get(f.id.countDistinct()).intValue() : 0;
-            int followingCount = result.get(f.id.countDistinct()) != null ? result.get(f.id.countDistinct()).intValue() : 0;
+            int postCount = result.get(qPost.id.countDistinct()) != null ? result.get(qPost.id.countDistinct()).intValue() : 0;
+            int followerCount = result.get(qFollowerFollow.id.countDistinct()) != null ? result.get(qFollowerFollow.id.countDistinct()).intValue() : 0;
+            int followingCount = result.get(qFollowingFollow.id.countDistinct()) != null ? result.get(qFollowingFollow.id.countDistinct()).intValue() : 0;
 
-            return MemberProfileSummaryDTO.toDTO(result.get(m.id).longValue(), result.get(m.nickname), result.get(m.profileImage), result.get(m.role), postCount, followerCount, followingCount);
+            return MemberProfileSummaryDTO.toDTO(result.get(qMember.id).longValue(), result.get(qMember.nickname), result.get(qMember.profileImage), result.get(qMember.role), postCount, followerCount, followingCount);
 
         }
     }
