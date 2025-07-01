d065e20d8ba2e3a93c0df865163717ec269bc690
이규민 <kyumin991105@duksung.ac.kr>
2025-07-02 01:03:20 +0900

    ♻️ [REFACTOR] 사용하지 않는 메소드, import 삭제

--- src/main/java/com/ceos/beatbuddy/domain/scrapandlike/repository/VenueReviewLikeRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/scrapandlike/repository/VenueReviewLikeRepository.java
@@ -3,9 +3,6 @@ package com.ceos.beatbuddy.domain.scrapandlike.repository;
 import com.ceos.beatbuddy.domain.scrapandlike.entity.VenueReviewLike;
 import com.ceos.beatbuddy.domain.scrapandlike.entity.VenueReviewLikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,6 +11,4 @@ import java.util.Optional;
 public interface VenueReviewLikeRepository extends JpaRepository<VenueReviewLike, VenueReviewLikeId> {
 
     boolean existsByVenueReview_IdAndMember_Id(Long venueReviewId, Long memberId);
-
-    Optional<Object> findByVenueReview_IdAndMember_Id(Long venueReviewId, Long memberId);
 }
