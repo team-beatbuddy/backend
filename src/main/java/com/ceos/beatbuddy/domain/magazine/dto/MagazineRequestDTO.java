70d3c60068f69266cdfbcc05871bda94399e5b38
이규민 <kyumin991105@duksung.ac.kr>
2025-07-06 19:50:38 +0900

    ✨ [FEAT] 작성 시, 필드 추가 및 유효성 검사 추가

--- src/main/java/com/ceos/beatbuddy/domain/magazine/dto/MagazineRequestDTO.java
+++ src/main/java/com/ceos/beatbuddy/domain/magazine/dto/MagazineRequestDTO.java
@@ -1,7 +1,10 @@
 package com.ceos.beatbuddy.domain.magazine.dto;
 
 import com.ceos.beatbuddy.domain.magazine.entity.Magazine;
+import com.ceos.beatbuddy.domain.magazine.exception.MagazineErrorCode;
 import com.ceos.beatbuddy.domain.member.entity.Member;
+import com.ceos.beatbuddy.global.CustomException;
+import com.ceos.beatbuddy.global.code.ErrorCode;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,16 +16,31 @@ public class MagazineRequestDTO {
     private String title;
     @Schema(description = "내용")
     private String content;
+    private boolean isPicked; // 픽된 매거진인지 여부
+    private boolean isSponsored; // 스폰서 매거진인지 여부
+    private boolean isPinned; // 고정된 매거진인지 여부
+    private boolean isVisible; // 띄워줄 매거진인지 여부
+    private int orderInHome; // 홈에서의 순서
+    private Long eventId;
 
     public static Magazine toEntity(MagazineRequestDTO dto, Member member) {
         return Magazine.builder()
                 .likes(0)
                 .views(0)
-                .isVisible(true)
-                .isPinned(false)
+                .isVisible(dto.isVisible())
+                .isPinned(dto.isPinned())
+                .isSponsored(dto.isSponsored())
+                .isPicked(dto.isPicked())
+                .orderInHome(dto.getOrderInHome())
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .member(member)
                 .build();
+    }
+
+    public void validatePinnedWithOrder() {
+        if (isPinned && orderInHome <= 0) {
+            throw new CustomException(MagazineErrorCode.INVALID_ORDER_IN_HOME);
+        }
     }
 }
