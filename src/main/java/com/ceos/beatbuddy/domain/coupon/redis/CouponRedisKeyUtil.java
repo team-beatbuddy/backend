62fd299d1ab8750ced4f1185fe24e74a64eda229
이규민 <kyumin991105@duksung.ac.kr>
2025-07-08 13:43:49 +0900

    ✨ [FEAT] venue 별 쿠폰 조회 구현

--- src/main/java/com/ceos/beatbuddy/domain/coupon/redis/CouponRedisKeyUtil.java
+++ src/main/java/com/ceos/beatbuddy/domain/coupon/redis/CouponRedisKeyUtil.java
@@ -5,9 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class CouponRedisKeyUtil {
 
-    public static String getQuotaKey(Long couponId, LocalDate date) {
-        return String.format("venue_coupon_quota:%d:%s",
-                couponId, date.format(DateTimeFormatter.BASIC_ISO_DATE));
+    public static String getQuotaKey(Long couponId, Long venueId, LocalDate date) {
+        return String.format("venue_coupon_quota:%d:%d:%s",
+                couponId, venueId, date.format(DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
 
