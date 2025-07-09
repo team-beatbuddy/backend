97b2e8a5a1fe452a56991071685b8f35448dc2db
이규민 <kyumin991105@duksung.ac.kr>
2025-07-09 13:48:49 +0900

    ✨ [FEAT] 성능 최적화를 위해 인덱스 추가

--- src/test/java/com/ceos/beatbuddy/BeatBuddyApplicationTests.java
+++ src/test/java/com/ceos/beatbuddy/BeatBuddyApplicationTests.java
@@ -1,9 +1,13 @@
 package com.ceos.beatbuddy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class BeatBuddyApplicationTests {
 
 }
