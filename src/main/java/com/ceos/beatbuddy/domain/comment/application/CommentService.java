d34fe674ffdfaf746afad1731d6944efce567873
이규민 <kyumin991105@duksung.ac.kr>
2025-07-09 14:34:01 +0900

    ✨ [FEAT] 게시글 댓글에 writerId, isFollowing 추가

--- src/main/java/com/ceos/beatbuddy/domain/comment/application/CommentService.java
+++ src/main/java/com/ceos/beatbuddy/domain/comment/application/CommentService.java
@@ -5,6 +5,7 @@ import com.ceos.beatbuddy.domain.comment.dto.CommentResponseDto;
 import com.ceos.beatbuddy.domain.comment.entity.Comment;
 import com.ceos.beatbuddy.domain.comment.exception.CommentErrorCode;
 import com.ceos.beatbuddy.domain.comment.repository.CommentRepository;
+import com.ceos.beatbuddy.domain.follow.repository.FollowRepository;
 import com.ceos.beatbuddy.domain.member.application.MemberService;
 import com.ceos.beatbuddy.domain.member.entity.Member;
 import com.ceos.beatbuddy.domain.post.application.PostService;
@@ -18,6 +19,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,6 +28,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final MemberService memberService;
     private final PostService postService;
+    private final FollowRepository followRepository;
 
     @Transactional
     public CommentResponseDto createComment(Long memberId, Long postId, CommentRequestDto requestDto) {
@@ -43,7 +47,9 @@ public class CommentService {
         Comment savedComment = commentRepository.save(comment);
         post.increaseComments();
 
-        return CommentResponseDto.from(savedComment, true); // 자신이 작성
+        boolean isFollowing = followRepository.existsByFollowerIdAndFollowingId(memberId, member.getId());
+
+        return CommentResponseDto.from(savedComment, true, isFollowing); // 자신이 작성
     }
 
     @Transactional
@@ -67,20 +73,28 @@ public class CommentService {
         Comment savedReply = commentRepository.save(reply);
         post.increaseComments();
 
-        return CommentResponseDto.from(savedReply, true); // 자신이 작성
+        boolean isFollowing = followRepository.existsByFollowerIdAndFollowingId(memberId, member.getId());
+
+        return CommentResponseDto.from(savedReply, true, isFollowing); // 자신이 작성
     }
 
     public CommentResponseDto getComment(Long commentId, Long memberId) {
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new CustomException(CommentErrorCode.COMMENT_NOT_FOUND));
-        return CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId)); // 자신이 작성한 댓글인지 여부
+
+        boolean isFollowing = followRepository.existsByFollowerIdAndFollowingId(memberId, comment.getMember().getId());
+        return CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId), isFollowing); // 자신이 작성한 댓글인지 여부
     }
 
     public Page<CommentResponseDto> getAllComments(Long postId, int page, int size, Long memberId) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Comment> comments = commentRepository.findAllByPost_Id(postId, pageable);
+
+        Set<Long> followingIds = followRepository.findFollowingMemberIds(memberId);
         return comments.map(comment ->
-            CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId))
+                    CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId),
+                            followingIds.contains(comment.getMember().getId())
+                    )
         );
     }
 
@@ -103,7 +117,9 @@ public CommentResponseDto updateComment(Long commentId, Long memberId, CommentRe
                 .likes(comment.getLikes())
                 .build();
 
-        return CommentResponseDto.from(commentRepository.save(updatedComment), true); // 자신이 작성한 댓글이므로 true
+        boolean isFollowing = followRepository.existsByFollowerIdAndFollowingId(memberId, comment.getMember().getId());
+
+        return CommentResponseDto.from(commentRepository.save(updatedComment), true, isFollowing); // 자신이 작성한 댓글이므로 true
     }
 
     @Transactional
@@ -127,7 +143,10 @@ public CommentResponseDto updateComment(Long commentId, Long memberId, CommentRe
         // 좋아요 로직 구현 필요 (중복 좋아요 방지 등)
         comment.increaseLike();
 
-        return CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId)); // 자신이 작성한 댓글인지 여부
+        // 팔로잉 여부
+        boolean isFollowing = followRepository.existsByFollowerIdAndFollowingId(memberId, comment.getMember().getId());
+
+        return CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId), isFollowing); // 자신이 작성한 댓글인지 여부
     }
 
     @Transactional
@@ -137,7 +156,8 @@ public CommentResponseDto updateComment(Long commentId, Long memberId, CommentRe
 
         // 좋아요 로직 구현 필요 (중복 좋아요 방지 등)
         comment.decreaseLike();
+        boolean isFollowing = followRepository.existsByFollowerIdAndFollowingId(memberId, comment.getMember().getId());
 
-        return CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId)); // 자신이 작성한 댓글인지 여부
+        return CommentResponseDto.from(comment, comment.getMember().getId().equals(memberId), isFollowing); // 자신이 작성한 댓글인지 여부
     }
 }
