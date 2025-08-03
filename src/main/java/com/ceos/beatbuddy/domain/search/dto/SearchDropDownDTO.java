e1eb211b5d8bbe174a88c0e2789d17746c803f48
이규민 <kyumin991105@duksung.ac.kr>
2025-08-03 14:40:56 +0900

    📝 [DOCS] 요청 상세 설명 작성

--- src/main/java/com/ceos/beatbuddy/domain/search/dto/SearchDropDownDTO.java
+++ src/main/java/com/ceos/beatbuddy/domain/search/dto/SearchDropDownDTO.java
@@ -16,26 +16,12 @@ import java.util.List;
 public class SearchDropDownDTO {
     private String keyword;
     @Schema(description = """
-        장르 필터 값 (예: '이디엠' 또는 'EDM')
-        - 이디엠, EDM
-        - 힙합, HIPHOP
-        - 알앤비, R&B
-        - 하우스, HOUSE
-        - 소울, 펑크, 소울앤펑크, SOUL&FUNK
-        - 테크노, TECHNO
-        - 케이팝, K-POP
-        - 팝, POP
-        - 라틴, LATIN
-        - 락, ROCK
+        장르 필터 값 (예: 'EDM' 또는 'HOUSE')
+        - HIPHOP,R&B,EDM,HOUSE,TECHNO,SOUL&FUNK,ROCK,LATIN,K-POP,POP
         """)
     private String genreTag;
     @Schema(description = """
-        지역 필터 값 (예: '홍대' 또는 'HONGDAE')
-        - 홍대, HONGDAE
-        - 이태원, ITAEWON
-        - 압구정, APGUJEONG
-        - 강남_신사, GANGNAM/SINSA
-        - 기타, OTHERS
+        지역 필터 값 (예: '홍대', '압구정', '강남/신사', '이태원', '기타')
         """)
     private String regionTag;
     private String sortCriteria;
