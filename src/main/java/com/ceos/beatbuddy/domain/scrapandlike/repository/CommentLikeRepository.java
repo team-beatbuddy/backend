82e6660b1f72a82a8d72fe36416593aead50d259
이규민 <kyumin991105@duksung.ac.kr>
2025-08-03 18:28:26 +0900

    댓글 삭제 시 좋아요 연관 데이터 버그 해결

--- src/main/java/com/ceos/beatbuddy/domain/scrapandlike/repository/CommentLikeRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/scrapandlike/repository/CommentLikeRepository.java
@@ -10,6 +10,9 @@ public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> 
     // 댓글 좋아요 여부 확인
     boolean existsByComment_IdAndMember_Id(Long commentId, Long memberId);
 
-    // 댓글 좋아요 삭제
+    // 댓글 좋아요 삭제 (특정 회원)
     void deleteByComment_IdAndMember_Id(Long commentId, Long memberId);
+    
+    // 댓글에 달린 모든 좋아요 삭제 (댓글 삭제 시 사용)
+    void deleteByComment_Id(Long commentId);
 }
