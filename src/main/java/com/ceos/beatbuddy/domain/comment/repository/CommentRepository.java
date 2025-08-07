7ab974aea32de1882f9958eba9051dff5c43b6b7
이규민 <kyumin991105@duksung.ac.kr>
2025-08-07 20:41:34 +0900

    🐛 [BUG]  댓글 삭제 시 대댓글 삭제 되도록 버그 수정

--- src/main/java/com/ceos/beatbuddy/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/comment/repository/CommentRepository.java
@@ -29,4 +29,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Modifying
     @Query("UPDATE Comment c SET c.likes = c.likes + 1 WHERE c.id = :commentId")
     void increaseLikesById(Long commentId);
+
+    List<Comment> findAllByReplyId(Long commentId);
 }
