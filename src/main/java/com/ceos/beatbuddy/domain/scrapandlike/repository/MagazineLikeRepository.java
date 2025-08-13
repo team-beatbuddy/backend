0dbdc5b0f34cae550485e63f1a9d2373c96ef66a
이규민 <kyumin991105@duksung.ac.kr>
2025-08-13 17:54:39 +0900

    fix: ãimport 추가

--- src/main/java/com/ceos/beatbuddy/domain/scrapandlike/repository/MagazineLikeRepository.java
+++ src/main/java/com/ceos/beatbuddy/domain/scrapandlike/repository/MagazineLikeRepository.java
@@ -2,6 +2,7 @@ package com.ceos.beatbuddy.domain.scrapandlike.repository;
 
 import com.ceos.beatbuddy.domain.scrapandlike.entity.MagazineLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
